3999e0111c38ec67647b9d6173d29d2165553fb3
lorphy <lorphy@139.com>
2025-09-24 14:16:21 +0800

    first try to work with wechat authorization

--- server.js
+++ server.js
@@ -7,13 +7,85 @@ const app= express();
 app.use(express.json());
 app.use(Cors());
 
+// 微信的登陆部分。 
+
+var AppID = 'wx30b2cee10d0cc93d';
+var AppSecret = '7cb468710e5c5b9e498d30807db12397';
+
+app.get('/wx_login', function(req,res, next){
+	console.log("oauth - login")
+	
+	// 第一步：用户同意授权，获取code
+	var router = 'get_wx_access_token';
+	// 这是编码后的地址
+	var return_uri = 'doctorlee-backend.onrender.com';  
+	var scope = 'snsapi_userinfo';
+	
+	res.redirect('https://open.weixin.qq.com/connect/oauth2/authorize?appid='+AppID+'&redirect_uri='+return_uri+'&response_type=code&scope='+scope+'&state=STATE#wechat_redirect');
+	
+});
+
+app.get('/get_wx_access_token', function(req,res, next){
+	console.log("get_wx_access_token")
+	//console.log("code_return: "+req.query.code)
+	// 第二步：通过code换取网页授权access_token
+	var code = req.query.code;
+	request.get(
+		{   
+			url:'https://api.weixin.qq.com/sns/oauth2/access_token?appid='+AppID+'&secret='+AppSecret+'&code='+code+'&grant_type=authorization_code',
+		},
+		function(error, response, body){
+			if(response.statusCode == 200){
+				
+				// 第三步：拉取用户信息(需scope为 snsapi_userinfo)
+				//console.log(JSON.parse(body));
+				var data = JSON.parse(body);
+				var access_token = data.access_token;
+				var openid = data.openid;
+				
+				request.get(
+					{
+						url:'https://api.weixin.qq.com/sns/userinfo?access_token='+access_token+'&openid='+openid+'&lang=zh_CN',
+					},
+					function(error, response, body){
+						if(response.statusCode == 200){
+							
+							// 第四步：根据获取的用户信息进行对应操作
+							var userinfo = JSON.parse(body);
+							//console.log(JSON.parse(body));
+							console.log('获取微信信息成功！');
+							
+							// 小测试，实际应用中，可以由此创建一个帐户
+							res.send("\
+								<h1>"+userinfo.nickname+" 的个人信息</h1>\
+								<p><img src='"+userinfo.headimgurl+"' /></p>\
+								<p>"+userinfo.city+"，"+userinfo.province+"，"+userinfo.country+"</p>\
+							");
+							
+						}else{
+							console.log(response.statusCode);
+						}
+					}
+				);
+			}else{
+				console.log(response.statusCode);
+			}
+		}
+	);
+});
+
+//微信登陆部分
+
+
 const port = process.env.PORT||8001;
 
 const uri = "mongodb+srv://lina:duuTmfCCz6DvF5pB@cluster0.tyj7k9i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 
 
-app.get("/",(req,res)=>res.status(200).send("Hello theweb"));
+app.get("/",(req,res)=>{
+	res.status(200).send("Hello theweb");}
+);
 
 async function getOneByName(name){
 	try{
@@ -85,5 +157,5 @@ app.get('/patients',async (req,res)=>{
 	}
 });
 
+app.listen(port,()=>{console.log(`Listening on localhost:${port}`)});
 
-app.listen(port,()=>console.log(`Listening on localhost:${port}`));
